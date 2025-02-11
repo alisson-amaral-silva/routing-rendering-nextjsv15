6f3305df65c90f9f31d657c5c76c6b5d728f6ca2
Alisson Amaral <aalissonamaral98@gmail.com>
2025-02-11 15:29:11 -0300

    add catch all route

--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from '@/components/NewsList/news-list'
-import { getNewsForYear } from '@/lib/utils'
-
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year
-  const news = getNewsForYear(newsYear)
-
-  return <NewsList news={news} />
-}
